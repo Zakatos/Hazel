804a5b1cc6754f751923346739cd03702b88826c
Zakatos <ameletiadis@yahoo.com>
2019-10-21 17:03:49 +0200

    Added KeyCodes & MouseButtonCodes.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,12 +10,15 @@ public:
 
 	void OnUpdate() override
 	{
-		HZ_INFO("ExampleLayer::Update");
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+		{
+			HZ_INFO("TAB KEY IS PRESSED");
+		}
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		HZ_TRACE("{0}", event);
+		//HZ_TRACE("{0}", event);
 	}
 };
 
